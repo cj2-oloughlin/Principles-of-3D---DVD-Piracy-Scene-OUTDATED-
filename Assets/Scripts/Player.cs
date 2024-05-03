6f75fa4cff558fc1dc6274b89f21244e52ff45d8
cj2oloughlin <caitlin2.oloughlin@live.uwe.ac.uk>
2024-05-03 12:39:10 +0100

    Redone playermovement

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,105 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] float mouseSensitivity = 3f;
+    [SerializeField] float movementSpeed = 20f;
+    [SerializeField] float jumpSpeed = 10f;
+    [SerializeField] float mass = 1f;
+    [SerializeField] float acceleration = 20f;
+    [SerializeField] Transform cameraTransform;
+
+    CharacterController controller;
+    Vector3 velocity;
+    Vector2 look;
+
+    PlayerInput playerInput;
+    InputAction moveAction;
+    InputAction lookAction;
+    InputAction jumpAction;
+    InputAction sprintAction;
+
+    void Awake()
+    {
+        controller = GetComponent<CharacterController>();
+        playerInput = GetComponent<PlayerInput>();
+        moveAction = playerInput.actions["move"];
+        lookAction = playerInput.actions["look"];
+        jumpAction = playerInput.actions["jump"];
+        sprintAction = playerInput.actions["sprint"];
+    }
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    public void Teleport(Vector3 position, Quaternion rotation)
+    {
+        transform.position = position;
+        Physics.SyncTransforms();
+        look.x = rotation.eulerAngles.y;
+        look.y = rotation.eulerAngles.z;
+        velocity = Vector3.zero;
+    }
+
+    void Update()
+    {
+        UpdateGravity();
+        UpdateMovement();
+        UpdateLook();
+    }
+
+    void UpdateGravity()
+    {
+        var gravity = Physics.gravity * mass * Time.deltaTime;
+        velocity.y = controller.isGrounded ? -1f : velocity.y + gravity.y;
+    }
+
+    Vector3 GetMovementInput()
+    {
+        var moveInput = moveAction.ReadValue<Vector2>();
+        var input = new Vector3();
+        input += transform.forward * moveInput.y;
+        input += transform.right * moveInput.x;
+        input = Vector3.ClampMagnitude(input, 1f);
+        var sprintInput = sprintAction.ReadValue<float>();
+        var multiplier = sprintInput > 0 ? 1.5f : 1f;
+        input *= movementSpeed * multiplier;
+
+        return input;
+    }
+
+    void UpdateMovement()
+    {
+        var input = GetMovementInput();
+
+        var factor = acceleration * Time.deltaTime;
+        velocity.x = Mathf.Lerp(velocity.x, input.x, factor);
+        velocity.z = Mathf.Lerp(velocity.z, input.z, factor);
+
+        var jumpInput = jumpAction.ReadValue<float>();
+        if (jumpInput > 0 && controller.isGrounded)
+        {
+            velocity.y += jumpSpeed;
+        }
+        
+        controller.Move(velocity * Time.deltaTime);
+    }
+
+    void UpdateLook()
+    {
+        var lookInput = lookAction.ReadValue<Vector2>();
+        look.x += lookInput.x * mouseSensitivity;
+        look.y += lookInput.y * mouseSensitivity;
+
+        look.y = Mathf.Clamp(look.y, -89f, 89f);
+
+        cameraTransform.localRotation = Quaternion.Euler(-look.y, 0, 0);
+        transform.localRotation = Quaternion.Euler(0, look.x, 0);
+    }
+}
