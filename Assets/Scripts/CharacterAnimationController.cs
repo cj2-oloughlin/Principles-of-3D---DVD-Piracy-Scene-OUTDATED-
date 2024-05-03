0122d7475f976f93eda112ca727557fef7399bbf
cj2oloughlin <caitlin2.oloughlin@live.uwe.ac.uk>
2024-05-03 16:57:01 +0100

    Redone animations
    Will be redoing button interactions and cutscene

---
+++ Assets/Scripts/CharacterAnimationController.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterAnimationController : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        //whisper should occur whenever the player presses 'E'
+        //run, sneak and yell takes place over a cutscene
+    }
+}
