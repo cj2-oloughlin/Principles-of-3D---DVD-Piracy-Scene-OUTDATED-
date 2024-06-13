336a3597cc71ed7d18422d67aa0aac17721e08c6
cj2oloughlin <caitlin2.oloughlin@live.uwe.ac.uk>
2024-06-13 14:40:06 +0100

    Interaction System

---
+++ Assets/Scripts/NPCInteractable.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class NPCInteractable : MonoBehaviour
+{
+    
+
+    public void Interact()
+    {
+        Debug.Log("Interact!");
+    }
+
+}
