e3196c602f7e633be033df9bfd96fa991f021899
cj2oloughlin <caitlin2.oloughlin@live.uwe.ac.uk>
2024-06-19 17:23:28 +0100

    Dialogue done

--- Assets/Scripts/Dialogue/TicketChecker.cs
+++ Assets/Scripts/Dialogue/TicketChecker.cs
@@ -19,10 +19,13 @@ public class TicketChecker : Dialogue
 
         var dialogData = new List<DialogData>();
 
-        dialogData.Add(new DialogData("Hi./wait:0.5//close/", "Ticket Checker", isSkipable: false));
-
-
-        dialogData.Add(new DialogData("Hi./wait:0.5//close/", "Ticket Checker", () => animator.SetBool("Talk", false), isSkipable: false));
+        dialogData.Add(new DialogData("/size:down//size:down//size:down/I'm just saying, Jessica isn't gonna make a move unless-/wait:0.5//close/", "Ticket Checker", isSkipable: false));
+        dialogData.Add(new DialogData("/size:init/.../wait:0.3//close/", "Ticket Checker", isSkipable: false));
+        dialogData.Add(new DialogData("What./wait:0.5//close/", "Ticket Checker", isSkipable: false));
+        dialogData.Add(new DialogData("Oh,/wait:0.2/ ugh. /wait:0.2/It's you./wait:0.5//close/", "Ticket Checker", isSkipable: false));
+        dialogData.Add(new DialogData("/speed:down/Sigh.../speed:init/ Ticket please./wait:0.5//close/", "Ticket Checker", isSkipable: false));
+        dialogData.Add(new DialogData("Okay, so,/wait:0.1/ please be aware that you will be escorted from the premises if you're recording./wait:0.2/ Any cameras will be confiscated./wait:0.5//close/", "Ticket Checker", isSkipable: false));
+        dialogData.Add(new DialogData("Enjoy.../wait:0.2/ I guess./wait:0.3/ /size:down//size:down//size:down/So Tony when're you gonna go talk to-/wait:0.2//close/", "Ticket Checker", () => animator.SetBool("Talk", false), isSkipable: false));
 
         DialogManager.Show(dialogData);
     }
